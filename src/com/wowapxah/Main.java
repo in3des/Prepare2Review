e6f2cd318e826a34a88573b4a512d5766b712b46
in3des <62700347+in3des@users.noreply.github.com>
2021-11-09 02:33:37 +0400

    relative path

--- src/com/wowapxah/Main.java
+++ src/com/wowapxah/Main.java
@@ -1,24 +1,33 @@
 package com.wowapxah;
 
+import java.io.File;
+import java.util.Collections;
+
 public class Main {
     /***
     ** путь к файлу с вопросами - изменить на свое
     ***/
-//    private static final String FILENAME = "/home/user/java/review/preproject-final.txt";
-//    private static final String FILENAME = "E:\\#WORK\\JM_Projects\\Prepare2Review\\preproject-final.txt";
-    private static final String FILENAME = "E:\\#WORK\\JM_Projects\\Prepare2Review\\sobes_core1.txt";
-//    private static final String FILENAME = "E:\\#WORK\\JM_Projects\\Prepare2Review\\sobes_core2.txt";
-//    private static final String FILENAME = "E:\\#WORK\\JM_Projects\\Prepare2Review\\sobes_multi.txt";
-//    private static final String FILENAME = "E:\\#WORK\\JM_Projects\\Prepare2Review\\sobes_sql.txt";
-//    private static final String FILENAME = "E:\\#WORK\\JM_Projects\\Prepare2Review\\sobes_hiber.txt";
-//    private static final String FILENAME = "E:\\#WORK\\JM_Projects\\Prepare2Review\\sobes_jdbc.txt";
-//    private static final String FILENAME = "E:\\#WORK\\JM_Projects\\Prepare2Review\\sobes_spring.txt";
-//    private static final String FILENAME = "E:\\#WORK\\JM_Projects\\Prepare2Review\\sobes_pattern.txt";
-//    private static final String FILENAME = "E:\\#WORK\\JM_Projects\\Prepare2Review\\sobes_algo.txt";
-//    private static final String FILENAME = "E:\\#WORK\\JM_Projects\\Prepare2Review\\sobes_dop.txt";
+
+//    private static final String FILENAME = "./preproject-final.txt";
+//    private static final String FILENAME = "./preproject-final.txt";
+//    private static final String FILENAME = "./sobes_core1.txt";
+//    private static final String FILENAME = "./sobes_core2.txt";
+//    private static final String FILENAME = "./sobes_multi.txt";
+//    private static final String FILENAME = "./sobes_sql.txt";
+//    private static final String FILENAME = "./sobes_hiber.txt";
+//    private static final String FILENAME = "./sobes_jdbc.txt";
+//    private static final String FILENAME = "./sobes_spring.txt";
+    private static final String FILENAME = "./sobes_spring.txt";
+//    private static final String FILENAME = "./sobes_pattern.txt";
+//    private static final String FILENAME = "./sobes_algo.txt";
+//    private static final String FILENAME = "./sobes_dop.txt";
 
     public static void main(String[] args) {
         Logic logic = new Logic(FILENAME);
         logic.run();
+
     }
+
+
+
 }
